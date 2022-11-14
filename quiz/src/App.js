6573187d336ab62f242c5d9077a22a4300337b2b
ivanbilotserkivskiy <63519782+ivanbilotserkivskiy@users.noreply.github.com>
2022-11-14 16:13:41 +0200

    quiz

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -3,6 +3,18 @@ import NewGame from './components/New_game';
 import React from 'react';
 
 function App() {
+  const [quiz,setQuiz] =React.useState([])
+
+
+  React.useEffect (()=>{
+ fetch('https://opentdb.com/api.php?amount=5&category=22&difficulty=easy')
+ .then(ref=>ref.json())
+ .then(data=>setQuiz(old=>old=data.results))
+ 
+  },[]) 
+
+
+  console.log(quiz)
   return (
     <div>
     <NewGame/>
