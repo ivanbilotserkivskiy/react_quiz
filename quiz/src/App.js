add8c2c8f06e007c5c46e820b804c42f2e819701
ivanbilotserkivskiy <63519782+ivanbilotserkivskiy@users.noreply.github.com>
2022-11-14 14:17:22 +0200

    start section

---
+++ quiz/src/App.js
@@ -0,0 +1,13 @@
+
+import NewGame from './components/New_game';
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+    <NewGame/>
+    </div>
+  );
+}
+
+export default App;
---
+++ quiz/src/components/New_game.js
@@ -0,0 +1,17 @@
+import React from "react";
+import style from '../index.css'
+export default function NewGame() {
+    return (
+        <div className="start-div">
+            <div className="figure_top">
+
+            </div>
+            <div className="figure-bottom">
+
+            </div>
+            <h2 className="start-title">Quizzical</h2>
+            <span className="start-text">Some description if needed</span>
+            <button className="start-button">Start quiz</button>
+        </div>
+    )
+}  
