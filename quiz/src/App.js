ac82dd355ee85e181bf02c818cf412d21e2d35fb
ivanbilotserkivskiy <63519782+ivanbilotserkivskiy@users.noreply.github.com>
2022-12-01 22:28:03 +0200

    work version

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -7,6 +7,7 @@ import {nanoid} from 'nanoid'
 function App() {
   const [quiz, setQuiz] =React.useState([])
   const [quize, showQuize] = React.useState(false)
+  const [newGame, setNewGame] = React.useState(false)
   React.useEffect (()=>{
  fetch('https://opentdb.com/api.php?amount=5&category=22&difficulty=easy&type=multiple')
  .then(ref=>ref.json())
@@ -16,20 +17,20 @@ function App() {
     isHeld:[false,false,false,false],
     id:[nanoid(),nanoid(),nanoid(),nanoid()],
     check:false,
+    guess:false
 
   }
  })).then(data=>setQuiz(data))
-
-
-
  
-  },[]) 
+  },[newGame]) 
+
+  let count = 0
 
   function toggleAnswers (id){
     setQuiz(p=>p.map((d)=>{
       let a = d.id.indexOf(id)
       let b = d.isHeld
-      if(a>-1){
+      if(a>-1&&!d.check ){
         b= d.isHeld.map((o,index)=>{
           return index===a?!o:false
         })
@@ -44,7 +45,7 @@ function App() {
  }
   
   const quizMarkup = quiz.map(x=>{
-    
+    x.incorrect_answers.indexOf(x.correct_answer)===-1 && x.incorrect_answers.splice(Math.floor(Math.random() * 3),0,x.correct_answer)
         return <Quiz 
         key={nanoid()}
         question={x.question}
@@ -71,12 +72,32 @@ function App() {
   // console.log(quiz)
 function checkAnswers() {
   setQuiz(old=>old.map(x=>{
+  let a =  false
+  x.isHeld.map((k,index)=> {
+      if(k&& x.incorrect_answers.indexOf(x.correct_answer)===index ){
+        return a=!x.guess
+      }
+
+  }) 
     return {
       ...x,
-      check:true
+      check:true,
+      guess:a
     }
   }))
+ 
 }
+quiz.map(x=>{
+ count = x.guess?count+1:count
+})
+console.log(count)
+
+
+  function startNewGame () {
+    setNewGame(old=>!old)
+    showQuize(old=>!old)
+  }
+
 
   return (
     <div className="quiz-div">
@@ -88,7 +109,9 @@ function checkAnswers() {
             </div>
    
     {quize?quizMarkup: <NewGame  isHide={quize} toogle={()=>toogle()}/>} 
-    {quize&&<button className="check-button" onClick={checkAnswers}>Check answers</button>}
+    {quize&&!quiz[0].check&&<button className="check-button" onClick={checkAnswers}>Check answers</button>}
+    {quize&&quiz[0].check&&<p>You scored {count}/5 correct answers </p>}
+    {quize&&quiz[0].check&&<button className="check-button play-again-button" onClick={startNewGame}>Play again</button>}
     </div>
   );
 }
